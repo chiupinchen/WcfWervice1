31d510d57b8e7539346c916ed675a43f6b761103
chiupin.chen at the office <cchen@children.org>
2014-02-14 11:50:07 -0600

    Moq

---
+++ AsyncFormTest/ExtensionMethods/Market.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncFormTest.ExtensionMethods
+{
+
+    public static class MyExtensionMethod
+    {
+        public static int GetTotalPrice(this Market market)
+        {
+            return market.Fruits.Sum(f => f.Price);
+        }
+
+        public static int GetTotalPrice(this Market market, int inflactionFactor)
+        {
+            return market.Fruits.Sum(f => f.Price) * inflactionFactor;
+        }
+
+        public static int GetTotalPriceUsingInterface(this ICalculatable calculatable)
+        {
+            return calculatable.Fruits.Sum(f => f.Price);
+        }
+    }
+
+    public class Fruit
+    {
+        public string Name;
+        public int Price;
+    }
+
+    public interface ICalculatable
+    {
+        List<Fruit> Fruits { get; set; }
+    }
+    public class Market : ICalculatable
+    {
+        private List<Fruit> fruits;
+
+        public Market()
+        { 
+        
+        }
+
+        public void Add(Fruit fruit)
+        {
+            fruits.Add(fruit);
+        }
+
+
+        public List<Fruit> Fruits
+        {
+            get
+            {
+                return fruits;
+            }
+            set
+            {
+                fruits = value;
+            }
+        }
+    }
+
+    public class ExtensionMethods
+    {
+        public ExtensionMethods()
+        {
+            Market market = new Market();
+            market.Add(new Fruit(){ Name = "Apple", Price = 5});
+            market.Add(new Fruit() { Name = "Orange", Price = 6 });
+
+            var totalPrice = market.GetTotalPrice();
+            var totalPrice2 = market.GetTotalPrice(5);
+            var totalPrice3 = market.GetTotalPriceUsingInterface();
+            var totalPrice4 = ((ICalculatable)market).GetTotalPriceUsingInterface();
+
+        }
+    }
+}
---
+++ MoqTest/Model.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqTest
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { set; get; }
+    }
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (10m / 100m * totalParam));
+        }
+    }
+
+    public class LinqValueCalculator : IValueCalculator
+    {
+        private IDiscountHelper discounter;
+        public LinqValueCalculator(IDiscountHelper discounterParam)
+        {
+            discounter = discounterParam;
+        }
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
+        }
+    }
+}
